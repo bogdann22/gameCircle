cb82eded8354c8ea3982eab584b8fc4811be6821
bogdann22 <bohdan2201200222231@gmail.com>
2022-11-13 20:10:39 +0200

    Добавьте файлы проекта.

---
+++ gameCircle/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace gameCircle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLabel.Location = new System.Drawing.Point(748, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(169, 33);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "Рахунок: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::gameCircle.Resource1.background1;
+            this.ClientSize = new System.Drawing.Size(980, 607);
+            this.Controls.Add(this.scoreLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Circle Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ gameCircle/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace gameCircle
+{
+    public partial class Form1 : Form
+    {
+        //змінні колів
+        public Bitmap HandlerTexture = Resource1.Hudler,
+                      TargetTexture = Resource1.Target;
+
+        private Point _targerPosition = new Point(300,300); // позиція точки в яку ми маємо попадати
+        private Point _direction = Point.Empty; //напрямок жовтого кола
+        private int _score = 0; //очки
+        private int speed = 10; //швидкість руху 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //плавніший бліки
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+            UpdateStyles();
+            Cursor.Hide();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //оновлення форми (рух колів)
+            Refresh();
+        }
+        //рух жовтого кола
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            timer2.Interval = r.Next(500, 1000);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            var localPosition = this.PointToClient(Cursor.Position); //позиція курсова
+
+
+            _targerPosition.X += _direction.X * speed;
+            _targerPosition.Y += _direction.Y * speed;
+
+            if(_targerPosition.X < 50 || _targerPosition.X > 950)
+            {
+                _direction.X *= -1;
+            }
+            if (_targerPosition.Y < 50 || _targerPosition.Y > 550)
+            {
+                _direction.Y *= -1;
+            }
+
+            Point between = new Point(localPosition.X - _targerPosition.X, localPosition.Y - _targerPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y));
+            if (distance < 20)
+            {
+                AddScore(1);
+            }
+
+
+            if (_score == 50)
+            {
+                speed = 15;
+            }
+            else if (_score == 100)
+            {
+                speed = 17;
+            }
+            else if (_score == 150)
+            {
+                speed = 22;
+            }else if (_score == 200)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                Cursor.Show();
+                System.Windows.MessageBox.Show("Вітаємо, ви виграли!", "Юхууу", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Close();
+            }
+
+            var handleRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100); // коло по кординамтам курсора
+            var targetRect = new Rectangle(_targerPosition.X - 50, _targerPosition.Y - 50, 100, 100); //координати жовтого кола
+            
+            //додавання колів на форму
+            g.DrawImage(TargetTexture, handleRect);
+            g.DrawImage(HandlerTexture, targetRect);
+        }
+
+        //додавання очків 
+        private void AddScore(int score)
+        {
+            _score += score;
+            scoreLabel.Text = "Рахунок: "  + _score.ToString();
+        }
+    }
+}
